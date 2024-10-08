c59645fd1e72b1e4947238a22eb0809cb4283498
leeduckgo <leeduckgo@gmail.com>
2024-10-08 10:10:25 +0800

    init

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,15 +3,15 @@ const CLIENT_ID = import.meta.env.VITE_GITHUB_OAUTH_CLIENT_ID
 export const getRedirectUri = () => {
   const location = window.location
   const searchParams = new URLSearchParams(location.search)
-  const redirectUri = searchParams.get('redirect_uri') || '/'
+  const redirectUri = '/'
   const url = `${location.origin}/auth/github/callback?redirect_uri=${encodeURIComponent(redirectUri)}`
   return url
 }
 
 export const githubOAuthUri = () => {
-  return `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${getRedirectUri()}&response_type=code&scope=user:email,repo`
+  return `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=https://board.gitfluid.rootmud.xyz/auth/github/callback&response_type=code&scope=user:email,repo`
 }
 
 export const githubOAuthContentUri = () => {
-  return `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${getRedirectUri()}&response_type=code&scope=user:email,repo&prompt=consent`
+  return `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=https://board.gitfluid.rootmud.xyz/auth/github/callback&response_type=code&scope=user:email,repo&prompt=consent`
 }
